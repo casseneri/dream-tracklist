5e0f611ce220d59766252f0190dce1127e3cc79c
casseneri <155483203+casseneri@users.noreply.github.com>
2026-07-10 12:04:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const TRACK_COUNT = 12;
 
-const tracks = document.getElementById("tracks");
+const tracksDiv = document.getElementById("tracks");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
+const nameInput = document.getElementById("name");
+
+// Load template image
+const template = new Image();
+template.src = "template.png";
+
+// Store all the song input boxes
+const trackInputs = [];
+
+// Create the inputs
 for (let i = 1; i <= TRACK_COUNT; i++) {
 
     const label = document.createElement("label");
@@ -11,7 +23,23 @@ for (let i = 1; i <= TRACK_COUNT; i++) {
     input.className = "trackInput";
     input.maxLength = 40;
 
-    tracks.appendChild(label);
-    tracks.appendChild(input);
+    tracksDiv.appendChild(label);
+    tracksDiv.appendChild(input);
+
+    trackInputs.push(input);
+
+    input.addEventListener("input", drawCanvas);
+}
+
+nameInput.addEventListener("input", drawCanvas);
+
+// Draw everything
+template.onload = drawCanvas;
+
+function drawCanvas() {
+
+    // Draw the background image
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(template, 0, 0, canvas.width, canvas.height);
 
 }
